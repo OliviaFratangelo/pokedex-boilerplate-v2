91ff25ac98ea1aec78545876e19141ffbf54a135
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-07-23 21:49:58 -0400

    First commit

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
 const express = require("express");
-const morgan = require("morgan");
-const pokeBank = require("./pokeBank");
-const pokeList = require("./views/pokeList");
-const pokeDetails = require("./views/pokeDetails");
-
 const app = express();
+const Pokemon = require("./models/Pokemon");
+const db = require("./db");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/", (req, res) => {
-  const pokemon = pokeBank.list();
-  res.send(pokeList(pokemon));
+app.get("/pokemon", async (req, res) => {
+  const pokemon = await Pokemon.findAll();
+  res.json(pokemon);
+})
+
+app.get("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon doesnt exist");
+  }
 });
 
-app.get("/pokemon/:id", (req, res) => {
-  const pokemon = pokeBank.find(req.params.id);
-  res.send(pokeDetails(pokemon));
+app.post("/pokemon", async (req, res) => {
+  const newPokemon = await Pokemon.create(req.body);
+  res.json(newPokemon);
+})
+
+app.put("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.update(req.body);
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
 });
 
-const PORT = 1337;
+app.delete("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.destroy();
+    res.status(204).send();
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
 
+(async () => {
+  try {
+    await db.sync();
+    console.log("Models synced with database");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`server is listening on port ${PORT}`);
 });
